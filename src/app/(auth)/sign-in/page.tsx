79434fe1b72b4c9f3a40bdcdc1aa54c78af30864
Ayushman <114122016@nitt.edu>
2025-09-13 18:09:26 +0530

    Second Day

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-views";
+
+function Page(){
+    return <>
+    
+    <SignInView/>
+    </>
+}
+export default Page;
