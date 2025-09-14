433ae4b3dbf551c916fe30124f6bedc25bfac4b5
Ayushman <114122016@nitt.edu>
2025-09-14 23:27:08 +0530

    3rd day part2

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -6,7 +6,11 @@ import { GeneratedAvatar } from "@/components/ui/generated-avata"
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react"
 import { auth } from "@/lib/auth"
 import { useRouter } from "next/navigation"
+import { Drawer,DrawerContent,DrawerDescription,DrawerFooter,DrawerHeader,DrawerTitle,DrawerTrigger } from "@/components/ui/drawer"
+import { useIsMobile } from "@/hooks/use-mobile"
+import { Button } from "@/components/ui/button"
 export const DashboardUeserButton = ()=>{
+    const isMobile = useIsMobile()
     const router = useRouter()
    const {data,isPending} = authClient.useSession()
    const onLogout = ()=>{
@@ -19,6 +23,44 @@ export const DashboardUeserButton = ()=>{
    }
    if(isPending || !data?.user){
     return <p>Loading...</p>
+   }
+   if(isMobile){
+    return (
+        <Drawer>
+            <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex itesm-center justufy-between bg-white/5 hover:bg-white/10 overflow-hidden">
+           {data.user.image? (<Avatar>
+                <AvatarImage src={data.user.image || undefined} alt={data.user.name || "User Avatar"}/>
+            </Avatar>) :<GeneratedAvatar seed={data.user.name} variant="initials" className="size-9 mr-3"/>}
+            <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                <p className="text-sm truncate w-full">
+                    {data.user.name}
+                </p>
+                <p className="text-xs truncate w-full">
+                    {data.user.email}
+                </p>
+            </div>
+            <ChevronDownIcon className="size-4 shrink-0"/>
+            </DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>
+                        {data.user.name}
+                    </DrawerTitle>
+                                           <DrawerDescription>
+                            {data.user.email}
+                        </DrawerDescription>
+                </DrawerHeader>
+                    <DrawerFooter>
+                        <Button variant="outline" onClick={()=>{}}>
+                           <CreditCardIcon className="size-4 text-black"/> Billing
+                        </Button>
+                         <Button variant="outline" onClick={onLogout}>
+                           <LogOutIcon className="size-4 text-black"/> LogOut
+                        </Button>
+                    </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+    )
    }
   return (
 
