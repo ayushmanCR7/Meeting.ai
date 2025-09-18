ec1479ac738151d240e68708ce4922248d13f537
Ayushman <114122016@nitt.edu>
2025-09-19 00:57:40 +0530

    Tired

--- src/app/api/stream-webhook/route.ts
+++ src/app/api/stream-webhook/route.ts
@@ -1,8 +1,9 @@
 import { db } from "@/db";
 import { agents, meetings } from "@/db/schems";
+import { inngest } from "@/inngest/client";
 import { streamVideo } from "@/lib/stream-video";
-import { CallSessionStartedEvent,CallSessionParticipantLeftEvent } from "@stream-io/node-sdk";
-import {  } from "@stream-io/video-react-sdk";
+import { CallSessionStartedEvent, CallSessionParticipantLeftEvent, CallEndedEvent, CallTranscriptionReadyEvent, CallRecording, CallRecordingReadyEvent } from "@stream-io/node-sdk";
+import { } from "@stream-io/video-react-sdk";
 import { and, eq, not } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -19,54 +20,88 @@ export async function POST(req: NextRequest) {
         );
     }
     const body = await req.text()
-    if(!verifySignatureWithSDK(body,signature)){
-        return NextResponse.json({error: "Invalid Signature"},{status: 401})
+    if (!verifySignatureWithSDK(body, signature)) {
+        return NextResponse.json({ error: "Invalid Signature" }, { status: 401 })
     }
     let payload: unknown;
-    try{
-      payload = JSON.parse(body) as Record<string,unknown>;
-    }catch{
-         return NextResponse.json({error: "Invalid JSON"},{status: 400})
+    try {
+        payload = JSON.parse(body) as Record<string, unknown>;
+    } catch {
+        return NextResponse.json({ error: "Invalid JSON" }, { status: 400 })
     }
-    const eventType = (payload as Record<string,unknown>)?.type
-    if(eventType === "call.session_started"){
-       const event = payload as CallSessionStartedEvent;
-       const meetingId = event.call.custom?.meetingId
+    const eventType = (payload as Record<string, unknown>)?.type
+    if (eventType === "call.session_started") {
+        const event = payload as CallSessionStartedEvent;
+        const meetingId = event.call.custom?.meetingId
         console.log(meetingId)
-       if(!meetingId){
-        return NextResponse.json({error: "Missing meetingId"}, {status: 400})
-       }
-      
-       const [existingMeeting] = await db.select().from(meetings).where(and(eq(meetings.id,meetingId),not(eq(meetings.status,"completed")),not(eq(meetings.status,"active")),not(eq(meetings.status,"cancelled")),not(eq(meetings.status,"processing"))))
-       console.log(existingMeeting)
-       if(!existingMeeting){
-        return NextResponse.json({error: "Metting Not found"},{status: 404})
-       }
-       
-       await db.update(meetings).set({
-        status: "active",startedAt: new Date()
-       }).where(eq(meetings.id,existingMeeting.id));
-       const [existingAgent] = await db.select().from(agents).where(eq(agents.id,existingMeeting.agentId))
-       if(!existingAgent){
-        return NextResponse.json({error: "Agnet not found"},{status: 404})
-       }
-        
-       const call = streamVideo.video.call("default",meetingId)
+        if (!meetingId) {
+            return NextResponse.json({ error: "Missing meetingId" }, { status: 400 })
+        }
 
-       const realTimeClient = await streamVideo.video.connectOpenAi({
-        call,openAiApiKey: process.env.OPEN_AI_API_KEY!,agentUserId:existingAgent.id
-       })
-       realTimeClient.updateSession({
-        instructions: existingAgent.instructions
-       })
-       }else if(eventType === "call.session_participant_left"){
+        const [existingMeeting] = await db.select().from(meetings).where(and(eq(meetings.id, meetingId), not(eq(meetings.status, "completed")), not(eq(meetings.status, "active")), not(eq(meetings.status, "cancelled")), not(eq(meetings.status, "processing"))))
+        console.log(existingMeeting)
+        if (!existingMeeting) {
+            return NextResponse.json({ error: "Metting Not found" }, { status: 404 })
+        }
+
+        await db.update(meetings).set({
+            status: "active", startedAt: new Date()
+        }).where(eq(meetings.id, existingMeeting.id));
+        const [existingAgent] = await db.select().from(agents).where(eq(agents.id, existingMeeting.agentId))
+        if (!existingAgent) {
+            return NextResponse.json({ error: "Agnet not found" }, { status: 404 })
+        }
+
+        const call = streamVideo.video.call("default", meetingId)
+
+        const realTimeClient = await streamVideo.video.connectOpenAi({
+            call, openAiApiKey: process.env.OPEN_AI_API_KEY!, agentUserId: existingAgent.id
+        })
+        realTimeClient.updateSession({
+            instructions: existingAgent.instructions
+        })
+    } else if (eventType === "call.session_participant_left") {
         const event = payload as CallSessionParticipantLeftEvent
         const meetingId = event.call_cid.split(":")[1];
-        if(!meetingId){
-            return NextResponse.json({error: "Missing meetingId"},{status: 400})
+        if (!meetingId) {
+            return NextResponse.json({ error: "Missing meetingId" }, { status: 400 })
         }
-        const call = streamVideo.video.call("default",meetingId)
+        const call = streamVideo.video.call("default", meetingId)
         await call.end()
     }
-    return NextResponse.json({status: "ok"})
+    else if (eventType === "call.session_ended") {
+        const event = payload as CallEndedEvent;
+        const meetingId = event.call.custom?.meetingId;
+        if (!meetingId) {
+            return NextResponse.json({ error: "Missing meetingId" }, { status: 400 })
+        }
+        await db.update(meetings).set({
+            status: "processing", endedAt: new Date()
+        }).where(and(eq(meetings.id, meetingId), eq(meetings.status, "active")))
+    }
+    else if (eventType === "call.transcription_ready") {
+        const event = payload as CallTranscriptionReadyEvent;
+        const meetingId = event.call_cid.split(":")[1]
+
+        const [updatedMeeting] = await db.update(meetings).set({ transcriptUrl: event.call_transcription.url }).where(eq(meetings.id, meetingId)).returning()
+        if (!updatedMeeting) {
+            return NextResponse.json({ error: "Meeting not found" }, { status: 400 })
+        }
+        await inngest.send({
+            name: "meetings/processing",
+            data: {
+                meetingId: updatedMeeting.id,
+                transcriptUrl: updatedMeeting.transcriptUrl,
+            }
+        })
+    }
+    else if (eventType === "call.recording_ready") {
+        const event = payload as CallRecordingReadyEvent;
+        const meetingId = event.call_cid.split(":")[1]
+
+        await db.update(meetings).set({
+            recordingUrl: event.call_recording.url
+        }).where(eq(meetings.id, meetingId))
+    }
+    return NextResponse.json({ status: "ok" })
 }
