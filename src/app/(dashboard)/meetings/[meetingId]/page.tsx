860eb0dbf6e396d9833011d22d1cefadbd7da9e0
Ayushman <114122016@nitt.edu>
2025-10-07 19:39:29 +0530

    Debugged Code

--- src/app/(dashboard)/meetings/[meetingId]/page.tsx
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -5,33 +5,43 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
-import { MeetingIdView, MeetingIdViewError, MeetingIdViewLoading } from "@/modules/meetings/ui/views/meeting-id-views";
+import { 
+  MeetingIdView, 
+  MeetingIdViewError, 
+  MeetingIdViewLoading 
+} from "@/modules/meetings/ui/views/meeting-id-views";
 
-interface Props{
-    params: Promise<{
-        meetingId: string
-    }>
+interface Props {
+  params: {
+    meetingId: string;
+  };
 }
-const Page = async({params}:Props)=>{
-    const {meetingId} = await params;
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
-    if(!session){
-        redirect("/sign-in")
-    }
-    const queryClient = getQueryClient()
-    void queryClient.prefetchQuery(
-        trpc.meetings.getOne.queryOptions({id: meetingId})
-    )
-    return <>
+
+const Page = async ({ params }: Props) => {
+  const { meetingId } = params;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
+  );
+
+  return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<MeetingIdViewLoading/>}>
-        <ErrorBoundary fallback = {<MeetingIdViewError/>}>
-          <MeetingIdView meetingId={meetingId}/>
+      <Suspense fallback={<MeetingIdViewLoading />}>
+        <ErrorBoundary fallback={<MeetingIdViewError />}>
+          <MeetingIdView meetingId={meetingId} />
         </ErrorBoundary>
       </Suspense>
     </HydrationBoundary>
-    </>
-}
-export default Page
+  );
+};
+
+export default Page;
